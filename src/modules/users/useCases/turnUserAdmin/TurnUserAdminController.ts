525a882881ec1ffc636bb4a447cc6f9d39682989
Lucas <lucasmbrute614@gmail.com>
2022-05-21 23:18:20 -0300

    feat: implemented TurnUserAdminController

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,21 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      this.turnUserAdminUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(404).json({
+        success: false,
+        message: error.message,
+      });
+    }
+
+    return response.status(200).json({
+      success: true,
+      message: `User transformed into a admin.`,
+    });
   }
 }
 
