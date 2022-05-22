b16cefa3271c561ae9f24da610d80d5b8911f964
Lucas <lucasmbrute614@gmail.com>
2022-05-21 23:00:53 -0300

    feat: implemented ListAllUsersController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,20 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const allUsers = this.listAllUsersUseCase.execute({
+        user_id: user_id.toString(),
+      });
+
+      return response.send(allUsers);
+    } catch (error) {
+      return response.json({
+        success: false,
+        message: error.message,
+      });
+    }
   }
 }
 
