b597184d95ac435bbcac9ac86177dc1e5bbc1eb9
Lucas <lucasmbrute614@gmail.com>
2022-05-24 23:25:58 -0300

    fix: return response with complete user instead instace date again in ShowUserProfileController

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -10,15 +10,7 @@ class ShowUserProfileController {
 
     try {
       const user = this.showUserProfileUseCase.execute({ user_id });
-      const { updated_at, created_at, ...rest } = user;
-
-      const userDateFormatted = {
-        rest,
-        updated_at: new Date(),
-        created_at: new Date(),
-      };
-
-      return response.status(200).json(userDateFormatted);
+      return response.status(200).json(user);
     } catch (error) {
       return response.status(404).json({ error });
     }
